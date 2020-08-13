df03cbe7562f691921b89f3641980edfc7173137
Duyun4722lex031 <alekseiduyun@yandex.ru>
2020-08-13 22:46:43 +0300

    les2.2

--- les2.py
+++ les2.py
@@ -1,4 +1,32 @@
 my_str = 'HELLO my Dear Friends' # Строки неизменяемый тип данных как и INT FlOAT BOOL
 
-for char reversed(my_str) 
+my_list = [1, 2, 3, 'HELLO', True, False, [1, 2, 3]] # Список list() изменяемый тип данных, коллекция
+
+my_tuple = [1, 2, 3, 'HELLO', True, False, [1, 2, 3]] # Кортеж tuple() неизменяемый тип, коллекция
+
+temp = str(n)
+t1 = temp + temp
+t2 = temp + temp + temp
+comp = n + int(t1) + int(t2)
+print("Результат равен:", comp)
+
+
+'''for char in reversed(my_str) :
     print(char)
+
+len_obj = len(my_str)
+
+while len_obj:
+    len_obj -= 1
+    char = my_str[len_obj]
+    print(char)
+
+
+iter_obj = my_str.__iter__()
+
+while True:
+    try:
+        char = next(iter_obj)
+    except StopIteration:
+        break
+    print(char)'''
